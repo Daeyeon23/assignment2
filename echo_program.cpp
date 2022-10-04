2523cab9dbd97bef59f9a30142a6eb06a038ccb4
DYKIM <init.rego0723@gmail.com>
2022-10-04 20:57:55 +0900

    exit update

--- echo_program.cpp
+++ echo_program.cpp
@@ -3,7 +3,16 @@
 using namespace std;
 
 int main() {
-	string A;
-	cin >> A;
-	cout << A;
+	
+	bool exit_flag = true;
+
+	while (exit_flag) {
+		string A;
+		cin >> A;
+		cout << A;
+		if (A == "exit") {
+			exit_flag = false;
+		}
+	}
+	
 }
